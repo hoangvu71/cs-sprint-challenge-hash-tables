091187f31b30832a2364ed9444a3162236371d26
Devin <hoangong71@gmail.com>
2020-10-09 16:51:56 -0700

    done ex1 ex2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,53 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
+#     Example:
+# ```
+#     input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+#     output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum equals 21 
+    # let's make a store dict
+    store = {}
+    # we can put weight as key to a dict, where value is the index
+    for index in range(len(weights)):
+        weight = weights[index]
+        if weight not in store:
+            store[weight] = index
+
+
+    # go through the list
+    result = []
+
+    # then see if the limit - weight exists in the dict
+    for index in range(len(weights)):
+        weight = weights[index]
+        weight_to_find = limit - weight
+        if weight_to_find in store:
+            result.append(weights[weight])
+            result.append(weight[weight_to_find])
+
+            
+    # if it does add the first weight and the second weight to an array
+    # then return the type as (array[0], array[1])
+    # make a function that we sort the index of the array
+    # make it called sort_array(array)
+    def sort_array(array):
+        if array[0] > array[1]:
+            return (array[0], array[1])
+        return (array[1], array[0])
+    
+    if len(result) == 2:
+        return sort_array(result)
+    # though before we need to check which array index is bigger so that we can return the tuple according to the readme
+    # which is higher valued index should be placed first
+
+
+
+
+
+
+
+
+
+
 
     return None
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -5,10 +5,72 @@ class Ticket:
         self.destination = destination
 
 
-def reconstruct_trip(tickets, length):
+def reconstruct_trip(tickets, length = 0):
     """
     YOUR CODE HERE
     """
     # Your code here
+    # example :
+#     tickets = [
+#     Ticket{ source: "PIT", destination: "ORD" },
+#     Ticket{ source: "XNA", destination: "CID" },
+#     Ticket{ source: "SFO", destination: "BHM" },
+#     Ticket{ source: "FLG", destination: "XNA" },
+#     Ticket{ source: "NONE", destination: "LAX" },
+#     Ticket{ source: "LAX", destination: "SFO" },
+#     Ticket{ source: "CID", destination: "SLC" },
+#     Ticket{ source: "ORD", destination: "NONE" },
+#     Ticket{ source: "SLC", destination: "PIT" },
+#     Ticket{ source: "BHM", destination: "FLG" }
+# ]
+
+    # reconstruct it, 
+    # put the source as key and destination as value into a dict
+    # make a dict
+    travel_dict = {}
+    for ticket in tickets:
+        if ticket.source not in travel_dict:
+            travel_dict[ticket.source] = ticket.destination
+    route = []
+    # make a recursive function
+    def get_destination(instance_dict):
+        print("instacne dict", instance_dict)
+        if instance_dict in travel_dict:
+            
+            route.append(travel_dict[instance_dict])
+            if travel_dict[instance_dict] == "NONE":
+                return None
+        return get_destination(route[-1])
+    
+    get_destination("NONE")
+    print("Route", route)
+    # which would take in a key
+    # then it would return the value from the dict
+    # then recurse it until the value from the dict is None
+    # meanwhile keep track of all the value from that function
+    # lets do this
+
+
+
+
+
+
 
     return route
+
+ticket_1 = Ticket("PIT", "ORD")
+ticket_2 = Ticket("XNA", "SAP")
+ticket_3 = Ticket("SFO", "BHM")
+ticket_4 = Ticket("FLG", "XNA")
+ticket_5 = Ticket("NONE", "LAX")
+ticket_6 = Ticket("LAX", "SFO")
+ticket_7 = Ticket("SAP", "SLC")
+ticket_8 = Ticket("ORD", "NONE")
+ticket_9 = Ticket("SLC", "PIT")
+ticket_10 = Ticket("BHM", "FLG")
+
+tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5,
+                   ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
+
+
+reconstruct_trip(tickets)
